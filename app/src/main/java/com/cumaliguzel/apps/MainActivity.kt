edd6181660cf88e67eab4464efc4d952201be7c5
Cumali Guzel <cumali.guzel@std.yeditepe.edu.tr>
2024-12-11 13:36:25 +0300

    bottom nav bar eklendi

--- app/src/main/java/com/cumaliguzel/apps/MainActivity.kt
+++ app/src/main/java/com/cumaliguzel/apps/MainActivity.kt
@@ -5,8 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
+import com.cumaliguzel.apps.screens.BestPage
+import com.cumaliguzel.apps.screens.FavoritesPage
 import com.cumaliguzel.apps.ui.theme.AppsTheme
 import com.cumaliguzel.apps.viewModel.ClothesViewModel
 import com.cumaliguzel.apps.viewModel.WeatherViewModel
@@ -37,10 +48,67 @@ class MainActivity : ComponentActivity() {
     @Composable
     private fun renderUI() {
         AppsTheme {
-            WeatherAndClothesPage(
+            MainScreen(
                 weatherViewModel = weatherViewModel,
                 clothesViewModel = clothesViewModel
             )
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    weatherViewModel: WeatherViewModel,
+    clothesViewModel: ClothesViewModel
+) {
+    var selectedTab by remember { mutableStateOf(0) } // Aktif sekmeyi takip etmek için
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                selectedTab = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+        }
+    ) { innerPadding ->
+
+        Box(modifier = Modifier.padding(innerPadding) ){
+            when (selectedTab) {
+            0 -> WeatherAndClothesPage(
+                weatherViewModel = weatherViewModel,
+                clothesViewModel = clothesViewModel
+            )
+            1 -> FavoritesPage()
+            2 -> BestPage()
+        }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(selectedTab: Int, onTabSelected: (Int) -> Unit) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        contentColor = MaterialTheme.colorScheme.onPrimary
+    ) {
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home") },
+            label = { Text("Home") },
+            selected = selectedTab == 0,
+            onClick = { onTabSelected(0) }
+        )
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorites") },
+            label = { Text("Favorites") },
+            selected = selectedTab == 1,
+            onClick = { onTabSelected(1) }
+        )
+        NavigationBarItem(
+            icon = { Icon(imageVector = Icons.Default.Star, contentDescription = "Best") },
+            label = { Text("Profile") },
+            selected = selectedTab == 2,
+            onClick = { onTabSelected(2) }
+        )
     }
 }
--- app/src/main/java/com/cumaliguzel/apps/data/Clothes.kt
+++ app/src/main/java/com/cumaliguzel/apps/data/Clothes.kt
@@ -3,5 +3,6 @@ package com.cumaliguzel.apps.data
 data class Clothes(
     val id : Int=0 ,
     val gender : String ="",
-    val img :String =""
+    val img :String ="",
+    val isFavorite : Boolean= false
 )
